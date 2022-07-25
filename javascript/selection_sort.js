f19be36a230ea463f7a996a27b9559b94f04f5bf
Breanne <BreMarotta@gmail.com>
2022-07-25 07:58:38 -0400

    javascript solved

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,14 @@
 function selectionSort(arr) {
-  // type your code here
+  let solution = [] //create new arr that will put in the numbers sorted
+
+  while(arr.length > 0) { //while loop that continues as long as there are still numbers in the array
+    const min = Math.min(...arr); //sets the minimum value in the arr to the variable min
+    const idx = arr.indexOf(min); //finds the index of min
+
+    solution.push(min); //pushes min to the end of the solution array
+    arr.splice(idx, 1); //removes min from original arr
+  }
+  return solution;
 }
 
 if (require.main === module) {
